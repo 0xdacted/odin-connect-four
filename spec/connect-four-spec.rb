12217b26c6349db941888b76d25b633c072e56b6
Red <crabmarket56@gmail.com>
2022-08-01 17:44:52 -0400

    Add place_symbol puts test

--- spec/connect-four-spec.rb
+++ spec/connect-four-spec.rb
@@ -41,15 +41,27 @@ describe ConnectFour do
     context 'when the selected column has at least one empty space' do
       it 'places the user\'s symbol in the first empty row within that column' do
         column = 2
-        row = 1
         board = piece.instance_variable_get(:@board)
         symbol = piece.instance_variable_get(:@player_one_symbol)
         board[0][column] = "O"
         piece.place_symbol(column, symbol, board)
         expect(board[0][column]).to eq("O")
-        expect(board[row][column]).to eq(symbol)
+        expect(board[1][column]).to eq(symbol)
+      end
+
+      it 'prints a confirmation of your placement and the state of the board after the placement' do
+        column = 2
+        row = 1
+        board = piece.instance_variable_get(:@board)
+        symbol = piece.instance_variable_get(:@player_one_symbol)
+        board[0][column] = "O"
+        confirmation_message = "You have placed your piece at column ##{column}, row ##{row}"
+        expect(piece).to receive(:puts).with(confirmation_message).once
+        expect(piece).to receive(:puts).with(board).once
+        piece.place_symbol(column, symbol, board)
       end
     end
+
 
   end
 end
