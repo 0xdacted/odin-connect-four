0e943ed8ae74c3585c57220561707c2519db23ba
Red <crabmarket56@gmail.com>
2022-08-01 16:22:11 -0400

    Add test for when user selects a full column and simplify testing

--- spec/connect-four-spec.rb
+++ spec/connect-four-spec.rb
@@ -1,7 +1,7 @@
 require './lib/connect-four.rb'
 
 describe Player do
-  describe 'place_piece' do
+  describe 'select_column' do
     subject(:turn) {described_class.new('Johnny, X')}
     context 'when user inputs a valid column with an open space' do
       before do
@@ -9,18 +9,36 @@ describe Player do
         allow(turn).to receive(:gets).and_return(valid)
       end
       it 'outputs messages informing the user that their selection was confirmed and where the piece was placed' do
+        board = Array.new(6) { Array.new(7) }
         name = turn.instance_variable_get(:@name)
-        board = [[nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil], [nil, "X", nil, nil, nil, nil]]
         turn_message = "#{name} it's your turn! Please input your desired column (1-8) and press enter"
         selection_message = "great choice #{name}, you have placed your piece in column 2"
         expect(turn).to receive(:puts).with(turn_message).once
         expect(turn).to receive(:puts).with(selection_message).once
-        expect(turn).to receive(:puts).with(board).once
-        turn.place_piece
+        turn.select_column(board)
       end
-
-     
     end
+
+    context 'when user inputs a valid column without an open space' do
+      before do
+        valid = 2
+        allow(turn).to receive(:gets).and_return(valid)
+      end
+      it 'outputs messages informing the user that the selected column is full' do
+        name = turn.instance_variable_get(:@name)
+        symbol = turn.instance_variable_get(:@symbol)
+        board = Array.new(6, Array.new(7))
+        board[0][1] = symbol
+        turn_message = "#{name} it's your turn! Please input your desired column (1-8) and press enter"
+        error_message = "Sorry #{name}, that column is full, please select another column"
+        expect(turn).to receive(:puts).with(turn_message).once
+        expect(turn).to receive(:puts).with(error_message).once
+        turn.select_column(board)
+      end
+    end
+end
+
+
 
     
 
